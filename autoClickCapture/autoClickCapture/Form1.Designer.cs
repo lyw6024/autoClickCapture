9c5c048b1a7472728fc4072e0caed5beef9c805b
malic <m6024m@163.com>
2021-02-20 14:25:35 +0800

    flex position

--- autoClickCapture/autoClickCapture/Form1.Designer.cs
+++ autoClickCapture/autoClickCapture/Form1.Designer.cs
@@ -29,78 +29,110 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.leftUpperX = new System.Windows.Forms.TextBox();
+            this.leftUpperY = new System.Windows.Forms.TextBox();
+            this.regionWidth = new System.Windows.Forms.TextBox();
+            this.regionHeight = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.regionHeight);
+            this.groupBox1.Controls.Add(this.regionWidth);
+            this.groupBox1.Controls.Add(this.leftUpperY);
+            this.groupBox1.Controls.Add(this.leftUpperX);
+            this.groupBox1.Controls.Add(this.checkBox1);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.numericUpDown3);
             this.groupBox1.Controls.Add(this.numericUpDown2);
             this.groupBox1.Controls.Add(this.numericUpDown1);
-            this.groupBox1.Controls.Add(this.button1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(367, 121);
+            this.groupBox1.Size = new System.Drawing.Size(367, 143);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
-            // button1
+            // checkBox1
             // 
-            this.button1.Location = new System.Drawing.Point(247, 34);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(79, 54);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(24, 113);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "开启截图";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
+            // label3
             // 
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 150);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(367, 678);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "截屏时延";
             // 
-            // numericUpDown1
+            // label2
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(98, 20);
-            this.numericUpDown1.Maximum = new decimal(new int[] {
-            999999,
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "点击间隔";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "点击次数";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(98, 85);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            1000,
             0,
             0,
             0});
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 21);
-            this.numericUpDown1.TabIndex = 1;
-            this.numericUpDown1.Value = new decimal(new int[] {
-            10,
-            0,
-            0,
-            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown3.TabIndex = 3;
             // 
             // numericUpDown2
             // 
             this.numericUpDown2.Location = new System.Drawing.Point(98, 53);
             this.numericUpDown2.Maximum = new decimal(new int[] {
-            2000,
+            9999999,
             0,
             0,
             0});
@@ -119,65 +151,141 @@
             0});
             this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
             // 
-            // numericUpDown3
+            // numericUpDown1
             // 
-            this.numericUpDown3.Location = new System.Drawing.Point(98, 94);
-            this.numericUpDown3.Maximum = new decimal(new int[] {
-            1000,
+            this.numericUpDown1.Location = new System.Drawing.Point(98, 20);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999999,
             0,
             0,
             0});
-            this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(120, 21);
-            this.numericUpDown3.TabIndex = 3;
-            this.numericUpDown3.Value = new decimal(new int[] {
-            10,
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
             0,
             0,
             0});
             // 
-            // label1
+            // button1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 22);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "点击次数";
+            this.button1.Location = new System.Drawing.Point(12, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label2
+            // pictureBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 55);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 12);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "点击间隔";
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 253);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(367, 641);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
-            // label3
+            // progressBar1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 94);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(53, 12);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "截屏时延";
+            this.progressBar1.Location = new System.Drawing.Point(12, 221);
+            this.progressBar1.Maximum = 3;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(366, 24);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(299, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(79, 54);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // leftUpperX
+            // 
+            this.leftUpperX.Location = new System.Drawing.Point(253, 39);
+            this.leftUpperX.Name = "leftUpperX";
+            this.leftUpperX.Size = new System.Drawing.Size(46, 21);
+            this.leftUpperX.TabIndex = 6;
+            this.leftUpperX.Text = "0";
+            // 
+            // leftUpperY
+            // 
+            this.leftUpperY.Location = new System.Drawing.Point(305, 39);
+            this.leftUpperY.Name = "leftUpperY";
+            this.leftUpperY.Size = new System.Drawing.Size(46, 21);
+            this.leftUpperY.TabIndex = 6;
+            this.leftUpperY.Text = "0";
+            // 
+            // regionWidth
+            // 
+            this.regionWidth.Location = new System.Drawing.Point(253, 87);
+            this.regionWidth.Name = "regionWidth";
+            this.regionWidth.Size = new System.Drawing.Size(46, 21);
+            this.regionWidth.TabIndex = 6;
+            this.regionWidth.Text = "0";
+            // 
+            // regionHeight
+            // 
+            this.regionHeight.Location = new System.Drawing.Point(305, 87);
+            this.regionHeight.Name = "regionHeight";
+            this.regionHeight.Size = new System.Drawing.Size(46, 21);
+            this.regionHeight.TabIndex = 6;
+            this.regionHeight.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(242, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "左上位置";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(242, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "区域宽高";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(253, 114);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "测试区域";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 840);
+            this.ClientSize = new System.Drawing.Size(393, 906);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -193,6 +301,16 @@
         private System.Windows.Forms.NumericUpDown numericUpDown3;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox regionHeight;
+        private System.Windows.Forms.TextBox regionWidth;
+        private System.Windows.Forms.TextBox leftUpperY;
+        private System.Windows.Forms.TextBox leftUpperX;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- autoClickCapture/autoClickCapture/Form1.cs
+++ autoClickCapture/autoClickCapture/Form1.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SQLite;
 using OpenCvSharp;
 
 namespace autoClickCapture
@@ -18,14 +18,16 @@ namespace autoClickCapture
     {
 
         BackgroundWorker bgClick,oncount;
+        SQLiteConnection m_dbConnection;
 
         public Form1()
         {
             InitializeComponent();
             bgClick = new BackgroundWorker();
-            bgClick.DoWork += new DoWorkEventHandler(clickbgwf);
             bgClick.WorkerReportsProgress = true;
-            bgClick.ProgressChanged += new ProgressChangedEventHandler(clickpcbgwf);
+            bgClick.WorkerSupportsCancellation = true;
+            bgClick.DoWork += new DoWorkEventHandler(clickbgwf);
+            bgClick.ProgressChanged += new ProgressChangedEventHandler(clickprogresscgbgwf);
             bgClick.RunWorkerCompleted += new RunWorkerCompletedEventHandler(
                 (object s,RunWorkerCompletedEventArgs e) => { setCapsValue(true);progressBar1.Value = 0; }) ;
 
@@ -46,7 +48,6 @@ namespace autoClickCapture
 
             oncount.RunWorkerCompleted += new RunWorkerCompletedEventHandler(
                 (s, e) => { bgClick.RunWorkerAsync((int)numericUpDown1.Value); }) ;
-
         }
 
         [DllImport("User32")]
@@ -81,17 +82,22 @@ namespace autoClickCapture
             }
             for (int i=0;i< endNumber; i++)
             {
+                if(bgClick.CancellationPending){break;}
                 Thread.Sleep((int)(numericUpDown2.Value - numericUpDown3.Value));
+                if (bgClick.CancellationPending) { break; }
                 AutoClick();
+                if (bgClick.CancellationPending) { break; }
                 Thread.Sleep((int)(numericUpDown3.Value));
+                if (bgClick.CancellationPending) { break; }
                 ((BackgroundWorker)sender).ReportProgress(0,i);
+                if (bgClick.CancellationPending) { break; }
             }
         }
 
         Bitmap lastb;
         Image lastImg;
         Mat lastsrc,lastdst;
-        private void clickpcbgwf(object sender,ProgressChangedEventArgs e)
+        private void clickprogresscgbgwf(object sender,ProgressChangedEventArgs e)
         {
             if (lastImg != null)
             {
@@ -111,25 +117,35 @@ namespace autoClickCapture
             }
             numericUpDown1.Value -= 1;
             Bitmap b = GetScreenCapture();
-            Mat src = OpenCvSharp.Extensions.BitmapConverter.ToMat(b);
-            Mat dst = new Mat();
-            src.ConvertTo(dst, MatType.CV_8UC1);
-            dst.SaveImage(String.Format("shot/cap{0:0000}.png", (int)e.UserState));
+            if(checkBox1.Checked)
+            {
+                Mat src = OpenCvSharp.Extensions.BitmapConverter.ToMat(b);
+                Mat dst = new Mat();
+                src.ConvertTo(dst, MatType.CV_8UC1);
+                dst.SaveImage(String.Format("shot/cap{0:0000}.png", (int)e.UserState));
+                lastsrc = src;
+                lastdst = dst;
+            }
             Image p = (Image)b;
             pictureBox1.BackgroundImage = p;
 
             lastImg = p;
             lastb = b;
-            lastsrc = src;
-            lastdst = dst;
         }
+
         private void setCapsValue(bool s)
         {
             button1.Enabled = s;
             numericUpDown1.Enabled = s;
             numericUpDown2.Enabled = s;
             numericUpDown3.Enabled = s;
+            leftUpperY.Enabled = s;
+            leftUpperX.Enabled = s;
+            regionWidth.Enabled = s;
+            regionHeight.Enabled = s;
+            button2.Enabled = !s;
         }
+
         private void button1_Click(object sender, EventArgs e)
         {
             setCapsValue(false);
@@ -137,12 +153,89 @@ namespace autoClickCapture
             oncount.RunWorkerAsync();
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bgClick.CancelAsync();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (lastImg != null)
+            {
+                lastImg.Dispose();
+            }
+            if (lastb != null)
+            {
+                lastb.Dispose();
+            }
+            Bitmap b = GetScreenCapture();
+            Image p = (Image)b;
+            pictureBox1.BackgroundImage = p;
+
+            lastImg = p;
+            lastb = b;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=uiparams.db;Version=3;");
+            m_dbConnection.Open();
+
+            Dictionary<string, string> uikvs = new Dictionary<string, string>();
+
+            string sql = "select * from param ";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                uikvs[ (string)reader["keyname"] ] = (string)reader["val"];
+            }
+
+            numericUpDown1.Value = Convert.ToDecimal(uikvs["clicktimes"]);
+            numericUpDown2.Value = Convert.ToDecimal(uikvs["clickperiod"]);
+            numericUpDown3.Value = Convert.ToDecimal(uikvs["shotdelay"]);
+            leftUpperX.Text = uikvs["upperleftx"];
+            leftUpperY.Text = uikvs["upperlefty"];
+            regionWidth.Text = uikvs["regionwidth"];
+            regionHeight.Text = uikvs["regionheight"];
+            m_dbConnection.Close();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            m_dbConnection = new SQLiteConnection("Data Source=uiparams.db;Version=3;");
+            m_dbConnection.Open();
+            Dictionary<string, string> uikvs = new Dictionary<string, string>();
+            string sql = "select * from param ";
+            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                uikvs[(string)reader["keyname"]] = (string)reader["val"];
+            }
+
+            uikvs["clicktimes"] = numericUpDown1.Value.ToString();
+            uikvs["clickperiod"] = numericUpDown2.Value.ToString();
+            uikvs["shotdelay"] = numericUpDown3.Value.ToString();
+            uikvs["upperleftx"] = leftUpperX.Text;
+            uikvs["upperlefty"] = leftUpperY.Text;
+            uikvs["regionwidth"] = regionWidth.Text;
+            uikvs["regionheight"] = regionHeight.Text;
+            foreach(var it in uikvs.Keys)
+            {
+                sql = String.Format("update param set val = '{0}' where keyname = '{1}'", uikvs[it], it);
+                command = new SQLiteCommand(sql, m_dbConnection);
+                command.ExecuteReader();
+            }
+            m_dbConnection.Close();
+        }
+
         private Bitmap GetScreenCapture()
         {
-            Rectangle tScreenRect = new Rectangle(0, 0, 565, 1000);
-            Bitmap tSrcBmp = new Bitmap(tScreenRect.Width, tScreenRect.Height); // 用于屏幕原始图片保存
+            Rectangle tScreenRect = new Rectangle(0, 0,Convert.ToInt32(regionWidth.Text), Convert.ToInt32(regionHeight.Text));
+            Bitmap tSrcBmp = new Bitmap(tScreenRect.Width, tScreenRect.Height); 
             Graphics gp = Graphics.FromImage(tSrcBmp);
-            gp.CopyFromScreen(0, 38, 0, 0, tScreenRect.Size);
+            gp.CopyFromScreen(Convert.ToInt32(leftUpperX.Text), Convert.ToInt32(leftUpperY.Text) , 0, 0, tScreenRect.Size);
             gp.DrawImage(tSrcBmp, 0, 0, tScreenRect, GraphicsUnit.Pixel);
             return tSrcBmp;
         }
